c65c283a35aec2b22c820843b56b366c39fa38e2
AbhiK57 <abhikhanduja243@gmail.com>
2025-07-13 00:00:55 -0400

    NCL peer initial code finished

--- cmd/ncl_peer/main.go
+++ cmd/ncl_peer/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"net"
 	"sync"
 
 	pb "github.com/AbhiK57/Lazy-NCL/proto/ncl"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -17,12 +20,12 @@ var (
 type server struct {
 	pb.UnimplementedNCLPeerServer
 	mu  sync.Mutex
-	log *pb.Metadata
+	log []*pb.Metadata
 }
 
 func NewServer() *server {
 	return &server{
-		log: &pb.Metadata{},
+		log: make([]*pb.Metadata, 0),
 	}
 }
 
@@ -35,4 +38,38 @@ func (s *server) AppendMetadata(ctx context.Context, req *pb.AppendMetadataReque
 	log.Printf("Appended metadata for record_id: %s. Log size: %d", req.Meta.RecordId, len(s.log))
 
 	return &pb.AppendMetadataResponse{}, nil
+}
+
+// streams metadata entries to a client
+func (s *server) ReadMetadata(req *pb.ReadMetadataRequest, stream pb.NCLPeer_ReadMetadataServer) error {
+	s.mu.Lock()
+	//copy relevant part of log
+	var streamEntries []*pb.Metadata
+	if req.Offset < int64(len(s.log)) {
+		streamEntries = s.log[req.Offset:]
+	}
+	s.mu.Unlock()
+
+	log.Printf("Streaming %d entries starting at offset %d", len(streamEntries), req.Offset)
+	for _, entry := range streamEntries {
+		if err := stream.Send(entry); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	log.Printf("NCL Peer service starting on port %d", *port)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterNCLPeerServer(s, NewServer())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve NCL peer server on GRPC: %v", err)
+	}
 }
