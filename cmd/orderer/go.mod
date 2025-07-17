c640fb80528181b72616f820e800dd475569081a
AbhiK57 <abhikhanduja243@gmail.com>
2025-07-17 17:35:45 -0400

    Finished out orderer

--- cmd/orderer/go.mod
+++ cmd/orderer/go.mod
@@ -2,8 +2,9 @@ module github.com/AbhiK57/Lazy-NCL/cmd/orderer
 
 go 1.24.4
 
+require github.com/AbhiK57/Lazy-NCL v0.0.0-20250717212711-390b8d1ed4ab
+
 require (
-	github.com/AbhiK57/Lazy-NCL v0.0.0-20250715030629-016a166a5edd
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
--- cmd/orderer/main.go
+++ cmd/orderer/main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"net"
 	"sync"
 	"time"
 
 	nclpb "github.com/AbhiK57/Lazy-NCL/proto/ncl"
+	ordererpb "github.com/AbhiK57/Lazy-NCL/proto/orderer"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
 )
 
 var (
 	nclLeaderAddr = flag.String("ncl_leader", "localhost:50051", "Address of the NCL leader peer")
 	pollInterval  = flag.Duration("poll_interval", 100*time.Millisecond, "Interval to poll NCL leader")
+	rpcPort       = flag.Int("port", 50081, "The RPC port for orderer service")
 )
 
 // stores resolved metadata for global sequence
@@ -26,6 +31,7 @@ type GlobalIndexEntry struct {
 // finalizes the global log order
 type Orderer struct {
 	nclClient nclpb.NCLPeerClient
+	ordererpb.UnimplementedOrdererServer
 
 	mu                sync.RWMutex
 	globalSequenceNum int64
@@ -99,6 +105,22 @@ func main() {
 
 	nclClient := nclpb.NewNCLPeerClient(conn)
 	orderer := NewOrderer(nclClient)
+
+	//start GRPC server async
+	go func() {
+		lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *rpcPort))
+		if err != nil {
+			log.Fatalf("Failed to listen on port %d: %v", *rpcPort, err)
+		}
+
+		s := grpc.NewServer()
+		ordererpb.RegisterOrdererServer(s, orderer)
+		reflection.Register(s)
+		log.Printf("Orderer RPC server listening at %v", lis.Addr())
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve RPC: %v", err)
+		}
+	}()
 
 	log.Println("Backround orderer begun. Polling NCL Leader...")
 	orderer.Run() //need to write function RUN
